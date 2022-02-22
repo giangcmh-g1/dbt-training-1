c4c8b158c57b5c211bec104d105f1bf2d2e45da4
HieuNM <hieunm@galaxy.one>
2022-02-22 14:35:50 +0700

    Add another person in lab1

--- models/lab1.sql
+++ models/lab1.sql
@@ -1,3 +1,4 @@
 select 1 as id, 'First person' as name
 union all select 2, 'Second person'
-union all select 3,'Third person'
+union all select 3, 'Third person'
+union all select 4, 'Fourth person'
